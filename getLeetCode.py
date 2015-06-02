c587cef46f63b450a78a8ed5294c8e73b9fc1c0a
zhang kang <zkdnfcf@163.com>
2015-06-02 11:02:53 +0800

    可下载leetcode存储到本地的python脚本

--- getLeetCode.py
+++ getLeetCode.py
@@ -1,23 +1,28 @@
 #coding:utf-8
+"""Copyright 2015 jimmy zhang
+write by jimmy zhang
+"""
+
+#/usr/bin/python2.4
 import urllib2
 import urllib
 import cookielib
 
 mydir = r'/home/jimmy/pythonTest/'
 myhost = r'https://leetcode.com'
+login = 'zkdnfcf'
+password = 'zkdnfcf386132'
 
+#由于leetcode会在打开的时候，通过cookie返回一个参数，后面post的时候会要用到。
 cj = cookielib.CookieJar()
 cookies = urllib2.HTTPCookieProcessor(cj)
 opener = urllib2.build_opener(cookies)
 opener.open('https://leetcode.com')
-
 csrftoken = ''
 for ck in cj:
     csrftoken = ck.value
 
-login = 'zkdnfcf'
-password = 'zkdnfcf386132'
-
+#post数据
 values = {'csrfmiddlewaretoken':csrftoken,'login':login,'password':password,'remember':'on'}
 values = urllib.urlencode(values)
 headers = {
@@ -26,17 +31,26 @@ headers = {
         'Referer':'https://leetcode.com/accounts/login/'
         }
 
-request = urllib2.Request("https://leetcode.com/accounts/login/",values,headers=headers)
-url = opener.open(request)
-page = url.read()
-print page
+def getPage():
+    """Get the problems Page 
+       模拟登陆后会自动跳转到problem页面，这里会显示出自己对于某个问题AC了没
+    """
+    global headers
+    global opener
+    global values
+    request = urllib2.Request("https://leetcode.com/accounts/login/",values,headers=headers)
+    url = opener.open(request)
+    page = url.read()
+    print page
 
 def saveCode(code,title):
+    """将AC代码存储到本地"""
     global mydir
     f = open(mydir+title+'.cpp','w')
     f.write(code)
 
 def downloadACcode(refer,codeadd,title):
+    """从leetcode页面将已AC代码存储到字符串里"""
     global headers
     global opener
     global myhost
@@ -59,6 +73,7 @@ def downloadACcode(refer,codeadd,title):
     saveCode(code,title)
 
 def findCode(address,title):
+    """找到AC代码所在的url"""
     global headers
     global opener
     global myhost
@@ -83,6 +98,7 @@ def findCode(address,title):
     downloadACcode(address,myhost+allSubmit[start+6:finish],title)
 
 def findAc(page):
+    """找AC过的问题"""
     index = 0
     while 1:
         index = page.find('class="ac"',index)
@@ -96,7 +112,5 @@ def findAc(page):
             findCode(myhost+page[start+9:finish],title)
         else:
             break
+getPage()
 findAc(page)
-
-#g = opener.open('https://oj.leetcode.com/submissions/')
-#print g.read()
